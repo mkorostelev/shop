495c26e29679fc02b61a3e2f52beee5c73083dc6
Maks Korostelov <kostul89@gmail.com>
2016-06-17 14:34:50 +0300

    Init rep

---
+++ my_task/app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    # byebug
+    if params["user"]["password"] != params["user"]["password_confirmation"]
+      render json: 'Wrong password confirmation', status: :unprocessable_entity
+      return
+      # return api_error(status: 401, errors: 'Wrong password confirmation')
+    end
+    params["user"]["token"] = SecureRandom.base64(64)
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :token, :password, :password_confirmation)
+    end
+end
